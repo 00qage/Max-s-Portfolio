11e79d4af7b76da7b5871540875c0aaded8cc624
00qage <z2369364@hotmail.com>
2018-05-02 00:15:52 +0800

    footer add Nivo-slider imgbox

--- js/all.js
+++ js/all.js
@@ -1,13 +1,13 @@
 $(document).ready(function () {
 
   //hamburger manu 漢堡選單
-$('#hamburgermanu').click(function (e) { 
-  e.preventDefault();
-  // $('body').delay(500).toggleClass('menu-show');
-  $('.header-menu ul').slideToggle(600);
-});
+  $('#hamburgermanu').click(function (e) {
+    e.preventDefault();
+    // $('body').delay(500).toggleClass('menu-show');
+    $('.header-menu ul').slideToggle(600);
+  });
 
-//scroll menu移動效果
+  //scroll menu移動效果
   var showSkill = false;
 
   $('.scrollTop').click(function (e) {
@@ -87,4 +87,31 @@ $('#hamburgermanu').click(function (e) {
   //   e.preventDefault();
   //   $('html,body').animate({scrollTop:0},800);
   // });
+
+  //Nivo slider
+  $(window).on('load', function () {
+    $('#slider').nivoSlider({
+      effect: 'fold', // Specify sets like: 'fold,fade,sliceDown' 
+      slices: 15, // For slice animations 
+      boxCols: 8, // For box animations 
+      boxRows: 4, // For box animations 
+      animSpeed: 500, // Slide transition speed 
+      pauseTime: 3000, // How long each slide will show 
+      startSlide: 0, // Set starting Slide (0 index) 
+      directionNav: false, // Next & Prev navigation 
+      controlNav: true, // 1,2,3... navigation 
+      controlNavThumbs: false, // Use thumbnails for Control Nav 
+      pauseOnHover: true, // Stop animation while hovering 
+      manualAdvance: false, // Force manual transitions 
+      prevText: 'Prev', // Prev directionNav text 
+      nextText: 'Next', // Next directionNav text 
+      randomStart: false, // Start on a random slide 
+      beforeChange: function () {}, // Triggers before a slide transition 
+      afterChange: function () {}, // Triggers after a slide transition 
+      slideshowEnd: function () {}, // Triggers after all slides have been shown 
+      lastSlide: function () {}, // Triggers when last slide is shown 
+      afterLoad: function () {} // Triggers when slider has loaded 
+    });
+
+  });
 });
